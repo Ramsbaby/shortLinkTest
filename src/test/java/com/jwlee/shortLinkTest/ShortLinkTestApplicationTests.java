dcef2211262766b4b0299afe46c2a55e501d9e55
JUNGWOOLEE <ms6698@naver.com>
2019-08-25 00:33:43 +0900

    0825 commit 테스트코드 정리

--- src/test/java/com/jwlee/shortLinkTest/ShortLinkTestApplicationTests.java
+++ src/test/java/com/jwlee/shortLinkTest/ShortLinkTestApplicationTests.java
@@ -3,6 +3,7 @@ package com.jwlee.shortLinkTest;
 import com.jwlee.shortLinkTest.common.util.Base62Util;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,10 +13,15 @@ import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
+        properties = {
+                "count=10"
+        },
         classes = {ShortLinkTestApplication.class},
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 public class ShortLinkTestApplicationTests {
+    @Value("${count}")
+    private int count;
 
     @Test
     public void contextLoads() {
@@ -24,7 +30,7 @@ public class ShortLinkTestApplicationTests {
 
     @Test
     public void testRandomEncodeAndDecode(){
-        for(int i=0; i < 10; i++) {
+        for(int i=0; i < count; i++) {
             Random rnd = new Random();
             long value = Math.abs(rnd.nextLong() % 10000000000L);
             System.out.println("VALUE : " + value);
